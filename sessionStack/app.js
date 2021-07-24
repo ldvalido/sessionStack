45488c24e15eb69d8d6ebb6a64d48908082ae361
Leandro Valido <leandro.valido@mercadolibre.com>
2021-07-23 22:59:24 -0300

    Initial Structure

--- sessionStack/app.js
+++ sessionStack/app.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const logger = require('morgan')
+const express = require('express');
+const logger = require('morgan');
 
-const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-const app = express()
+const port = 5000;
 
-const port = 5000
+const app = express();
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-app.listen(port, function() {
-    console.log('App listing on port 5000!')
-})
+app.listen(port, function () {
+  console.log('App listening on port 5000!');
+});
 
-module.exports = app
+module.exports = app;
--- sessionStack/routes/users.js
+++ sessionStack/routes/users.js
@@ -1,20 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   const data = {
-    name: 'Victor Jaha',
-    job: 'Tech Author'
+    name: 'Victor Jonah',
+    job: 'Technical Author',
   };
-
   res.status(200).send(data);
 });
 
-router.post('/', function(req, res, next) {
-  const {name, job} = res.post();
-  const data = {name, job}
+/* Add user to the list. */
+router.post('/', function (req, res, next) {
+  const { name, job } = req.post();
+  const data = {
+    name,
+    job
+  }
   res.status(200).send(data);
-})
+});
 
 module.exports = router;
